ed3f438d3adac01f4c041dd717c86120537de9e1
Rome Bell <romebell5@gmail.com>
2020-08-13 14:34:36 -0700

    [passport] Add ppConfig

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,17 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+// secret: What we actually giving the user to use our site / session cookie
+// resave: Save the session even if it's modified, make this false
+// saveUninitialized: if we have a new session, we'll save it, therefore,
+// setting this to true
+
+app.use(session({
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -19,6 +32,8 @@ app.get('/profile', (req, res) => {
 });
 
 app.use('/auth', require('./routes/auth'));
+
+
 
 
 const port = process.env.PORT || 3000;
